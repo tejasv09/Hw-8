2162ad98fe5ce723c106c41c97d77008756832cb
tejasv09 <35947354+tejasv09@users.noreply.github.com>
2018-04-23 18:11:11 -0700

    Add files via upload

---
+++ helperfunctions.py
@@ -0,0 +1,12 @@
+def add(n,m):    
+    return n+m
+   
+def diff(n,m):    
+    return n-m
+def product(n,m):    
+    return n*m
+def greatest(n,m):    
+    if n>m:
+    	return n
+    else:
+        return m
---
+++ main.py
@@ -0,0 +1,3 @@
+import helper 
+import sys
+print helper.add(int(sys.argv[1]),int(sys.argv[2]))
